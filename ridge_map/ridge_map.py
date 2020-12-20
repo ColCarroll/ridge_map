e7331d5038e3dee99aa061d562a8661cac6b5d62
Colin <ColCarroll@users.noreply.github.com>
2020-12-19 23:15:10 -0500

    Fix link to fonts on gh
    thanks @flxrhn!

--- ridge_map/ridge_map.py
+++ ridge_map/ridge_map.py
@@ -29,7 +29,7 @@ class FontManager:
 
     def __init__(
         self,
-        github_url="https://github.com/google/fonts/blob/master/ofl/cinzel/static/Cinzel-Regular.ttf?raw=True",  # pylint: disable=line-too-long
+        github_url="https://github.com/google/fonts/blob/master/ofl/cinzel/static/Cinzel-Regular.ttf?raw=true",  # pylint: disable=line-too-long
     ):
         """
         Lazily download a font.
@@ -38,7 +38,7 @@ class FontManager:
         ----------
         github_url : str
             Can really be any .ttf file, but probably looks like
-            "https://github.com/google/fonts/blob/master/ofl/cinzel/Cinzel-Regular.ttf?raw=True"
+            "https://github.com/google/fonts/blob/master/ofl/cinzel/Cinzel-Regular.ttf?raw=true"
         """
         self.github_url = github_url
         self._prop = None
